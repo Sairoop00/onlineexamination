d37daa0cb7f3a278889e7e606a8adb5166a71417
Sairoop00 <65495546+Sairoop00@users.noreply.github.com>
2021-02-12 03:25:21 +0000

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('ec2-18-204-74-74.compute-1.amazonaws.com','adkzlfboirgmdw','5e1cd8822a827e657d0b9d3e1585afb08e0cb7d9989b9039416969eb8db66011','dd532lh7c1453p')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('sql213.epizy.com','adkzlfboirgmdw','jy3M2svjH7mcLb','epiz_27911649_Project')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
